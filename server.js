90169720a8c373cb816a7572ab9a3c7e432cc122
elliottdombrowski <elliottroyal@me.com>
2021-10-27 15:37:19 -0500

    Set up port/initializing express + some cookies info in serverjs

---
+++ server.js
@@ -0,0 +1,40 @@
+//REQUIRE NODEY STUFF
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+//REQUIRE MYSQL PACKAGES/CONNECTION ROUTES
+const sequelize = require ('./config/connection');
+const SequelizeStore = require('connect-session-sequelize');
+
+//DECLARE EXPRESS AND A PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//SET UP HANDLEBARS W/ CUSTOM HELPERS
+const hbs = exphbs.create({ helpers });
+
+//COOKIES STUFF
+const sess = {
+    secret: 'SUPER secret secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+//TELL EXPRESS TO USE TEMPLATE ENGINE
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//CHANGE FORCE TO FALSE BEFORE DEPLOYMENT
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on ${PORT}`))
+});
