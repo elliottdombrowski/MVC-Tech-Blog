74de474ff66a6c749d1390b71d4a08e461770407
elliottdombrowski <elliottroyal@me.com>
2021-11-25 14:36:07 -0800

    todo-fix all that broken css

---
+++ public/js/script.js
@@ -0,0 +1,16 @@
+const signInBtn = document.getElementById('signIn');
+const signUpBtn = document.getElementById('signUp');
+const signupForm = document.getElementById('signup-form');
+const loginForm = document.getElementById('login-form');
+const container = document.querySelector('.container');
+
+signInBtn.addEventListener('click', () => {
+    container.classList.remove('right-panel-active');
+});
+
+signUpBtn.addEventListener('click', () => {
+    container.classList.add('right-panel-active');
+});
+
+signupForm.addEventListener('submit', (e) => e.preventDefault());
+loginForm.addEventListener('submit', (e) => e.preventDefault());
