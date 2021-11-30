b67383cd229d1b03625a2c89b9af8bb65fdd9038
elliottdombrowski <elliottroyal@me.com>
2021-11-29 23:57:47 -0800

    added very barebones form handler for blog submit

--- public/js/profilePage.js
+++ public/js/profilePage.js
@@ -1,5 +1,27 @@
-const handleProfileData = (e) => {
-    console.log('made it this far');
+// HANDLER FOR POST SUBMIT FORM
+const blogPostHandler = async (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById('title').value.trim();
+    const body = document.getElementById('post-body').value.trim();
+
+    if (title && body) {
+        //FINISH THIS LATER
+        const response = await fetch('/api/???', {
+            method: 'POST',
+            body: JSON.stringify({ title, body }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            //DO WHAT? DOCUMENT LOCATION REPLACE?
+        } else {
+            alert(response.statusText);
+        }
+    }
 };
 
-document.getElementById('profile-deets').addEventListener('click', handleProfileData);
+
+document
+    .querySelector('#blog-submit')
+    .addEventListener('submit', blogPostHandler);
