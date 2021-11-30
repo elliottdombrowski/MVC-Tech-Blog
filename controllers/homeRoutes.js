8f553e4a157fc1bda32cd7a55846cfe507e2fedd
elliottdombrowski <elliottroyal@me.com>
2021-11-29 23:08:25 -0800

    edited profile information page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,21 +18,21 @@ router.get('/login', async (req, res) => {
     }
 });
 
-// router.get('/profile', withAuth, async (req, res) => {
-//     try {
-//         res.render('profile', {logged_in: req.session.logged_in});
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
-
 router.get('/profile', withAuth, async (req, res) => {
+    try {
+        res.render('profile', {logged_in: req.session.logged_in});
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/user', withAuth, async (req, res) => {
     try {
         const userData = await User.findByPk(req.session.user_id);
         
         const user = userData.get({ plain: true });
 
-        res.render('profile', {
+        res.render('user', {
             ...user,
             logged_in: req.session.logged_in
         });
