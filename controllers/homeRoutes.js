b9c5a4ebcf7c08dbc6b6077ab41801e36fda982c
elliottdombrowski <elliottroyal@me.com>
2021-11-29 20:06:38 -0800

    gotta fix signup profile bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -34,9 +34,8 @@ router.get('/profile', withAuth, async (req, res) => {
 
         res.render('profile', {
             ...user,
-            logged_in: true
+            logged_in: req.session.logged_in
         });
-        console.log('hitting render');
     } catch (err) {
         res.status(500).json(err);
     }
--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,7 @@ const loginFormHandler = async (e) => {
         });
 
         if(response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
@@ -35,7 +35,7 @@ const signupFormHandler = async (e) => {
         }); 
 
         if(response.ok) {
-            document.location.replace('/profile');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
