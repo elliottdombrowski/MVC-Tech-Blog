5f05d6ae65f4c1c543417eea4f92c076e3184b05
elliottdombrowski <elliottroyal@me.com>
2021-12-02 15:58:08 -0600

    added comment route stuff

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,6 +2,36 @@ const showComments = () => {
     document.location.replace('/comments')
 };
 
-document
+const postComment = (e) => {
+    e.preventDefault();
+
+    const comment = document.querySelector('#comment-input');
+
+    if (comment) {
+        //FINISH THIS ROUTE
+        const res = await fetch('/api/comment/', {
+            method: 'POST',
+            body: JSON.stringify({ comment }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (res.ok) {
+            
+        } else {
+            alert('Please enter a comment.');
+        }
+    }
+};
+
+//WRAP QUERY SELECTORS IN TRY/ CATCH TO PREVENT ERRORS IF NOT FOUND
+try {
+    document
     .querySelector('#comment-btn')
     .addEventListener('click', showComments);
+} catch {}
+
+try {
+    document
+        .querySelector('#comment-submit')
+        .addEventListener('submit', postComment);
+} catch {}
