6856b681d12343858bbc859c3ff2f79e6de5cad3
Elliott Dombrowski <elliottroyal@me.com>
2021-12-05 20:55:45 -0600

    first commit to heroku

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 let sequelize;
 
+//COMMENTING SO I CAN COMMIT TO HEROKU. 
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
